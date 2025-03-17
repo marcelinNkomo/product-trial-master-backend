3913f5956bd355bb9e6877dd121809ca9d29ce2f
Marcelin (MNK) <marcelin.arsene.nkomo@gmail.com>
2025-03-17 16:58:44 +0100

    init repository

---
+++ src/main/java/com/alten/producttrialmasterbackend/ProductTrialMasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alten.producttrialmasterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductTrialMasterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductTrialMasterBackendApplication.class, args);
+	}
+
+}
